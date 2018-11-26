2106e6f88fdc0532215c1cae0441274dde724aeb
DerekPeacock <derek.peacock@yahoo.com>
2018-11-26 10:47:00 +0000

    Added access db and sql create table

---
+++ TutorialDB/CreateEmployees.sql
@@ -0,0 +1,8 @@
+﻿-- Create a new table called 'Employees' in schema 'dbo'
+CREATE TABLE dbo.Employees
+(
+   EmployeesId  INT    NOT NULL   PRIMARY KEY, 
+   Name      [NVARCHAR](50)  NOT NULL,
+   Location   [NVARCHAR](50)  NOT NULL
+);
+GO
