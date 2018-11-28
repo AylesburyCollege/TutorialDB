6b587820f5ca68abb21de9e77a5336d21adcf89e
DerekPeacock <derek.peacock@yahoo.com>
2018-11-28 15:40:37 +0000

    All data added for Branches

--- SQLQuery Insert Branch.sql
+++ SQLQuery Insert Branch.sql
@@ -1,5 +1,14 @@
 ﻿INSERT INTO Branches (BranchID, Street, Area, City, PostCode, PhoneNo, FaxNo)
---VALUES ('B2','56 Clover Dr',NULL,'London','NW10 6EU','0181-963-1030','0181-453-7992');
---VALUES ('B3','163 Main Street','Partick','Glasgow','G11 9QX','0141 339 2178','0141033904439');
-VALUES ('B4','32 Manse Road','Leigh','Bristol',
-'BS99 1NZ','0117-916-1170','0117-776-1114');
+VALUES ('B2','56 Clover Dr',NULL,'London','NW10 6EU','0181-963-1030','0181-453-7992');
+
+INSERT INTO Branches (BranchID, Street, Area, City, PostCode, PhoneNo, FaxNo)
+VALUES ('B3','163 Main Street','Partick','Glasgow','G11 9QX','0141 339 2178','0141033904439');
+
+INSERT INTO Branches (BranchID, Street, Area, City, PostCode, PhoneNo, FaxNo)
+VALUES ('B4','32 Manse Road','Leigh','Bristol','BS99 1NZ','0117-916-1170','0117-776-1114');
+
+INSERT INTO Branches (BranchID, Street, Area, City, PostCode, PhoneNo, FaxNo)
+VALUES ('B5','22 Deer Street','Sidcup','London','SW1 4EH','0171-886-1212','0171-886-1214');
+
+INSERT INTO Branches (BranchID, Street, Area, City, PostCode, PhoneNo, FaxNo)
+VALUES ('B7','16 Argyll Street','Dyce','Aberdeen','AB2 3SU','01234 67125','01234-67111');
